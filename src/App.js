ea1f549be258aba806f6f8cef4fbade3443822df
BenjamienSD <benjamien.deprez.dtp@gmail.com>
2020-04-13 17:06:10 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1>hello</h1>
+      <h1>Hello World</h1>
     </div>
   );
 }
